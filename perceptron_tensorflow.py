01cdbe933b1b9feefd7b85f92afb643f59539236
Sergey Vlasovv <35797418+SergeyVlasov@users.noreply.github.com>
2018-04-17 15:55:35 +0300

    Update perceptron_tensorflow.py

--- perceptron_tensorflow.py
+++ perceptron_tensorflow.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 
 train_inp_data = np.array([
-    [0, 0, 0, 0, 0],
+    [0.1, 0.1, 0.1, 0.1, 0.1],
     [0, 0.5, 1, 0.5, 0],
     [0, 0.2, 0.5, 0.2, 0],
     [1, 1, 1, 1, 1]
@@ -22,8 +22,8 @@ x_example = np.array([
 ])
 
 
-learning_rate = 0.1
-epoch = 5000
+learning_rate = 0.01
+epoch = 10000
 n_input = 5
 n_hidden = 10
 n_output = 1
@@ -37,39 +37,36 @@ Weights_hid_to_out = tf.Variable(tf.random_uniform([n_hidden, n_output], -1.0, 1
 bias_hid = tf.Variable(tf.ones([n_hidden], name="Bias_hiden"))
 bias_out = tf.Variable(tf.ones([n_output], name="Bias_output"))
 
-layer_hid = tf.sigmoid(tf.matmul(X, Weights_inp_to_hid) + bias_hid)
-out = tf.sigmoid(tf.matmul(layer_hid, Weights_hid_to_out) + bias_out)
+L2 = tf.sigmoid(tf.matmul(X, Weights_inp_to_hid) + bias_hid)
+probability_dropout = tf.placeholder(tf.float32)
+L2 = tf.nn.dropout(L2, probability_dropout)
 
-cost_func = tf.losses.log_loss(Y, out) # croosentropy  tf.reduce_mean(-Y * tf.log(hy) - (1 - Y) * tf.log(1 - hy))
-#cost_func = tf.losses.mean_squared_error  # other variant of cost
 
-optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost_func)
-''' other variant '''
-#optimizer = tf.train.adadeltaoptimizer(learning_rate).minimize(cost_func)
-#optimizer = tf.train.RMSPropOptimizer(self.lr).minimize(self.loss)
+out = tf.sigmoid(tf.matmul(L2, Weights_hid_to_out) + bias_out)
 
+
+cost = tf.reduce_mean(-Y * tf.log(out) - (1 - Y) * tf.log(1 - out)) # croosentropy
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
 
 init = tf.global_variables_initializer()
 
 with tf.Session() as session:
     session.run(init)
-     
-    #weights before training    
-    #print(session.run(Weights_inp_to_hid))
+
+    #print(session.run(W1))
 
     for step in range(epoch):
-        session.run(optimizer, feed_dict={X: train_inp_data, Y: label_train})
-     # print cost
+        session.run(optimizer, feed_dict={X: train_inp_data, Y: label_train, probability_dropout: 1})
+     # print cost:
         if step % 1000 == 0:
-            print(session.run(cost_func, feed_dict={X: train_inp_data, Y: label_train}))
+            print(session.run(cost, feed_dict={X: train_inp_data, Y: label_train, probability_dropout: 1}))
 
     answer = tf.equal(tf.floor(out + 0.5), Y)
     accuracy = tf.reduce_mean(tf.cast(answer, "float"))
 
-    print(session.run([out], feed_dict={X: train_inp_data, Y: label_train}))
-    print("Accuracy:", accuracy.eval({X: train_inp_data, Y: label_train}))
+    print(session.run([out], feed_dict={X: train_inp_data, Y: label_train, probability_dropout: 1}))
+    print("Accuracy:", accuracy.eval({X: train_inp_data, Y: label_train, probability_dropout: 1}))
 
-    print(session.run([out], feed_dict = {X: x_example}))
-    
-    #weights after training 
-    #print(session.run(Weights_inp_to_hid))
+    print(session.run([out], feed_dict = {X: x_example, probability_dropout: 1}))
+
+    #print(session.run(W1))
